32ae3ec1e8fd7591eed7cea82e1c25ed5ea28bc0
Hozzi <ghwls0905@naver.com>
2020-07-19 14:04:50 +0900

    modify

--- Day02/app/src/main/java/history/Test14_CustomView03.java
+++ Day02/app/src/main/java/history/Test14_CustomView03.java
@@ -3,6 +3,9 @@ package history;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -20,30 +23,66 @@ public class Test14_CustomView03 extends AppCompatActivity {
 
     @BindView(R.id.phone_book) ListView phone_book;
     @BindView(R.id.total_num) TextView total_num;
-
+    @BindView(R.id.search_board) EditText search_board;
     Test14_Custom_Adapter adapter;
+    ArrayList<Phone_Item> list = new ArrayList<>();
+    ArrayList<Phone_Item> temp = new ArrayList<>();
+    String text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test14__custom_view03);
         ButterKnife.bind(this);
+        getList();
+        filter(search_board.getText().toString());
+        search_board.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-        adapter = new Test14_Custom_Adapter(this,getList());
+            }
 
-        phone_book.setAdapter(adapter);
-        total_num.setText("총"+getList().size()+"개의 연락처");
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                text = search_board.getText().toString();
+                filter(text);
+            }
+        });
+
+        link_adapter(temp);
     }
 
-    private ArrayList<Phone_Item> getList(){
+    private void getList(){
         String names[] = getResources().getStringArray(R.array.phone_name);
         String nums[] = getResources().getStringArray(R.array.phone_num);
-
-        ArrayList<Phone_Item> list = new ArrayList<>();
         for(int i=0; i<names.length; i++){
             list.add(new Phone_Item(names[i],nums[i]));
         }
-        return list;
     }
 
+    private void filter(String text){
+        temp.clear();
+        if(text.length() == 0){
+            temp.addAll(list);
+        }else{
+            for(int i=0; i<list.size(); i++){
+                if(list.get(i).getUser_name().contains(text)){
+                    temp.add(new Phone_Item(list.get(i).getUser_name(),list.get(i).getUser_num()));
+                }
+            }
+        }
+    }
+
+
+    private void link_adapter(ArrayList<Phone_Item> array){
+        adapter = new Test14_Custom_Adapter(this,array);
+        phone_book.setAdapter(adapter);
+        total_num.setText("총"+array.size()+"개의 연락처");
+        adapter.notifyDataSetChanged();
+    }
 
 }
